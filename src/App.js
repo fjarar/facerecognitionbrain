d40ebaffd8e894331d897e0cab95474585b555b9
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-11-27 15:05:27 -0300

    Added ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import React, {Component} from 'react';
-import Particles from 'react-particles-js';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
-const particlesOptions = {  
-    particles: {
-      number: {
-        value: 100,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      }
-    }  
-}
-
-class App extends Component{
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-    }
+      input: "",
+    };
   }
 
-  onInputChange = (event)=>{
-    console.log(event);
-  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*<FaceRecognition />*/}
       </div>
     );
-  }  
+  }
 }
 
 export default App;
