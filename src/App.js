4d57de560080ed0e6256a2714c9dd3e7ea2f975a
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-11-27 13:04:32 -0300

    Fixed a particles problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
 
 
 const particlesOptions = {  
     particles: {
-      line_linked: {
-        shadow: {
+      number: {
+        value: 100,
+        density: {
           enable: true,
-          color: "#3CA9D1",
-          blur: 5
+          value_area: 800
         }
       }
     }  
 }
+
 function App() {
   return (
     <div className="App">
-      <Particles
+      <Particles className='particles'
         params={particlesOptions}
       />
       <Navigation />
