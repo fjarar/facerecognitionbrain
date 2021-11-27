8f5a6027853f8361f27366b6edbd64e886759779
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2021-11-27 12:47:36 -0300

    Changed particles.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+
+const particlesOptions = {  
+    particles: {
+      line_linked: {
+        shadow: {
+          enable: true,
+          color: "#3CA9D1",
+          blur: 5
+        }
+      }
+    }  
+}
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
       <Rank />
