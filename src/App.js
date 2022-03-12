205fb1ea22eab369da70bbc926c710354918c6c2
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2022-03-12 20:46:52 -0300

    added sign in from back to front

--- src/App.js
+++ src/App.js
@@ -110,13 +110,13 @@ class App extends Component {
   render() {
     const {isSignedIn, box, route, imageUrl}=this.state;
     const particlesInit = (main) => {
-      console.log(main);
+      //console.log(main);
   
       // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
     };
   
     const particlesLoaded = (container) => {
-      console.log(container);
+      //console.log(container);
     };
     return (
       <div className="App">
--- src/components/SingIn/SignIn.js
+++ src/components/SingIn/SignIn.js
@@ -18,8 +18,21 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    console.log(this.state);
-    this.props.onRouteChange('home')
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data === 'success'){
+          this.props.onRouteChange('home')
+        }
+      })
+    
   }
 
   render(){
@@ -40,6 +53,7 @@ class SignIn extends React.Component {
                   type="email"
                   name="email-address"
                   id="email-address"
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -52,6 +66,7 @@ class SignIn extends React.Component {
                   type="password"
                   name="password"
                   id="password"
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
